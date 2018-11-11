3e1ce432ac751d8613c59939985ca3d55644bd11
Bence Sebők <bence.sebok.personal@gmail.com>
2018-11-11 18:25:44 +0100

    Add DHT22 library

---
+++ software/gecko/thesis_gecko/inc/DHT22.h
@@ -0,0 +1,14 @@
+/*
+ * DHT22.h
+ *
+ *  Created on: Nov 11, 2018
+ *      Author: sebokbence
+ */
+
+#ifndef INC_DHT22_H_
+#define INC_DHT22_H_
+
+#define DHT22_DATA_PORT (gpioPortC)
+#define DHT22_DATA_PIN (6U)
+
+#endif /* INC_DHT22_H_ */
